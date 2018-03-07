0b16b2a46dcbaf62b15c739fc16fdb9a0a94ac35
Clayton Mansfield <cltnmansfield5@gmail.com>
2018-03-07 09:43:32 -0700

    removeDuplicateLps
    This is a Visual Studio solution folder containing some hypothetical code to remove duplicate lps from the Epicor database. This will be a potential starter for a customization for Starlight development.

---
+++ RemoveDuplicateLPs/RemoveDuplicateLPs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace RemoveDuplicateLPs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Somehow create array of LPs with same lot number - don't know Starlight software enough to do this
+            string[] lps = { "X1", "X2", "X10", "X11", "X100", "X101" };
+            //Remove LPs of smaller length
+            Array.Sort(lps, (x, y) => x.Length.CompareTo(y.Length));
+
+            int maxLength = lps[lps.Length - 1].Length;
+
+            for (int i = 0; i < lps.Length; i++)
+            {
+                if (lps[i].Length < maxLength) {
+                    // delete LP at lps[i]
+                    lps = lps.Where(w => w != lps[i]).ToArray();
+                }
+            }
+            //Of LPs remaining, sort alphabetically, then delete all but the last
+
+            Array.Sort(lps);
+
+            for (int i = 0; (i-1) < lps.Length; i++)
+            {
+                //delete LP at lps[i]
+                lps = lps.Where(w => w != lps[i]).ToArray();
+            }
+        }
+    }
+}
